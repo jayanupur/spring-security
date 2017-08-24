0fba4f636bf5f75d02ea14a3ab756fbef07c86ea
jayanupur <jayanupur7@gmail.com>
2017-08-24 15:48:13 +0530

    FIRST COMMIT

---
+++ src/main/java/com/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,73 @@
+package com.hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected admin page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/adminprofile**", method = RequestMethod.GET)
+	public ModelAndView adminProfile() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is adminprofile page!");
+		model.setViewName("adminprofile");
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/guest**", method = RequestMethod.GET)
+	public ModelAndView guestPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is guest page!");
+		model.setViewName("guest");
+
+		return model;
+
+	}
+	
+	
+	@RequestMapping(value = "/user**", method = RequestMethod.GET)
+	public ModelAndView usersPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is user page!");
+		model.setViewName("user");
+
+		return model;
+
+	}
+
+
+
+}
